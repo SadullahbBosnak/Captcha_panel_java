4435ed1d8c750aa04f70e38196b2178db40b224f
sadullah b <sadullahbosnak.hotmail.com>
2024-07-27 17:44:43 +0300

    first commit

---
+++ baslat.java
@@ -0,0 +1,21 @@
+package yenid;
+
+import yenid.CaptchaPanel;
+
+import javax.swing.*;
+
+public class baslat {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Captcha Panel");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(300, 250);
+                frame.setLocationRelativeTo(null);
+                frame.add(new CaptchaPanel());
+                frame.setVisible(true);
+            }
+        });
+    }
+}
